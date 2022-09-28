12aa94e0948025eabe13d7e1b8070ad84f37ac8e
Aweke <awekebeldagmail.com>
2022-09-28 19:50:19 -0400

    DOM Manipulation

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Do your magic here!
- 
 
-
+// Function to change webpage background color
+function set_background() {
+  let userColor = document.getElementById("container");
+  userColor.style.background= 'red'
+}
